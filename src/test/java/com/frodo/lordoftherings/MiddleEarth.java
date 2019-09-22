2ce04278a5356dd339e8f48acbb4f02b4b8dd6ab
Frodo <Frodo@Bagend.com>
2019-09-22 14:21:56 +0200

    Added some more json

--- src/test/java/com/frodo/lordoftherings/MiddleEarth.java
+++ src/test/java/com/frodo/lordoftherings/MiddleEarth.java
@@ -26,4 +26,32 @@ public class MiddleEarth {
         assertEquals("bagend", bagend);
 
     }
+
+    @Test
+    public void shouldMapToGenericObject () throws IOException {
+
+        //given
+        String jsonString = "{\"name\":\"frodo\", \"address\":\"bagend\"} ";
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Object obj = mapper.readValue(jsonString, Object.class);
+        System.out.println("obj type: " + obj.getClass().toString()); // java.util.LinkedHashMap
+        System.out.println("obj: " + obj);
+    }
+
+    @Test
+    public void shouldMapToSpecificObject () throws IOException {
+
+        //given
+        String jsonString = "{\"name\":\"frodo\", \"address\":\"bagend\"} ";
+        ObjectMapper mapper = new ObjectMapper();
+
+        // parsing JSON to concrete Object
+        MiddleEarthResident middleEarthResident = mapper.readValue(jsonString, MiddleEarthResident.class);
+        System.out.println("middleEarthResident: " + middleEarthResident);
+        System.out.println("middleEarthResident.author: " + middleEarthResident.getName());
+        System.out.println("middleEarthResident.address: " + middleEarthResident.getAddress());
+        System.out.println("middleEarthResident.obj class: " + middleEarthResident.getClass()); //com.frodo.lordoftherings.MiddleEarthResidentp
+    }
 }
